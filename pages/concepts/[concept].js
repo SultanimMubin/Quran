1295dad9f41a107a77bda4cf14892bc1162aef31
Saeed Nemati <sn.1361@gmail.com>
2021-09-10 11:14:26 +0430

    committed by script

---
+++ pages/concepts/[concept].js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+import Error from 'next/error';
+const marked = require("marked");
+import { useRouter } from 'next/router'
+
+const index = ({ urlSegments, content, errorCode, type }) => {
+    
+    const router = useRouter()
+    // set title from <h1> or first line with # in markdowns
+    // include disqus maybe
+
+    if (errorCode) {
+        return <Error statusCode={errorCode} />
+    }
+
+    return <>
+        <div onClick={() => router.push('/')} className="p-5 cursor-pointer">Home</div>
+        <div className={type + (type === 'markdown' ? ' prose pl-10 mb-10 pt-4' : '')} dangerouslySetInnerHTML={{ __html: content }}></div>
+    </>
+}
+
+export async function getServerSideProps({ params, res }) {
+    const urlSegments = params.concept || [];
+    const diskSegments = [process.cwd(), 'contents', 'concepts'].concat(urlSegments);
+    var filePath = path.join.apply(null, [...diskSegments, 'index.html']);
+    if (!fs.existsSync(filePath)) {
+        filePath = path.join.apply(null, [...diskSegments, 'index.md']);
+    }
+    if (!fs.existsSync(filePath)) {
+        filePath = path.join.apply(null, [...diskSegments]) + '.md';
+    }
+    if (!fs.existsSync(filePath)) {
+        filePath = path.join.apply(null, [...diskSegments]) + '.html';
+    }
+    console.log(filePath);
+    if (!fs.existsSync(filePath)) {
+        res.statusCode = 404;
+        const result = { props: { urlSegments: urlSegments, errorCode: 404 } }
+        return result;
+    }
+    else {
+        var type = 'html';
+        try {
+            var content = fs.readFileSync(filePath, 'utf8');
+            if (filePath.endsWith('.md')) {
+                type = 'markdown';
+                if (content.charCodeAt(0) == 65279) {
+                    content = content.slice(1);
+                }
+                content = marked(content);
+            }
+            const result = { props: { urlSegments: urlSegments, content: content, type } };
+            return result;
+        } catch (e) {
+            console.log(e);
+            res.statusCode = 500;
+            const result = { props: { urlSegments: urlSegments, errorCode: 500 } }
+            return result;
+        }
+    }
+}
+
+export default index;
+
+// algabra => 
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import { useRouter } from 'next/router'
 
 const Index = ({ chaptersJson }) => {
 
+    const [tab, setTab] = useState('chapters');
+
     const router = useRouter()
 
     const goToChapter = (chapter) => {
@@ -13,18 +15,29 @@ const Index = ({ chaptersJson }) => {
     }
 
     return <>
-        <h1 className="py-4 flex items-center justify-center text-5xl font-bold">Quran</h1>
-        <ul id='chapters' className="flex flex-wrap flex-grow mx-12 mb-4 justify-center">
-            {
-                chaptersJson.map(
-                    chapter => <li key={chapter.chapter_number} onClick={() => goToChapter(chapter)} className="m-1 py-2 px-4 bg-green-200 rounded cursor-pointer hover:bg-green-900 hover:text-white transition-all duration-200">
-                        <div className="text-xs font-bold">{chapter.chapter_number}</div>
-                        <div className="my-1 font-medium">{chapter.name_simple}</div>
-                        <div className="text-xs text-right">{chapter.name_arabic}</div>
-                    </li>
-                )
-            }
-        </ul>
+        <h1 className="py-4 flex items-center justify-around text-5xl font-bold">
+            <span className='cursor-pointer' onClick={() => router.push('/concepts/index')}>Concepts</span>
+            <span className='cursor-pointer' onClick={() => setTab('chapters')}>Surahs</span>
+        </h1>
+        {
+            tab === 'chapters'
+                ?
+                <ul id='chapters' className="flex flex-wrap flex-grow mx-12 mb-4 justify-center">
+                    {
+                        chaptersJson.map(
+                            chapter => <li key={chapter.chapter_number} onClick={() => goToChapter(chapter)} className="m-1 py-2 px-4 bg-green-200 rounded cursor-pointer hover:bg-green-900 hover:text-white transition-all duration-200">
+                                <div className="text-xs font-bold">{chapter.chapter_number}</div>
+                                <div className="my-1 font-medium">{chapter.name_simple}</div>
+                                <div className="text-xs text-right">{chapter.name_arabic}</div>
+                            </li>
+                        )
+                    }
+                </ul>
+                :
+                <ul>
+                    <li>hi</li>
+                </ul>
+        }
     </>
 }
 
