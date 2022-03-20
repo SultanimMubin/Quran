fad5d6e64a687dcba705c8f3dc82adbb45bc67a4
Saeed Nemati <sn.1361@gmail.com>
2022-03-20 20:07:27 +0330

    committed by script

--- pages/[chapter]/[verse].js
+++ pages/[chapter]/[verse].js
@@ -11,7 +11,7 @@ const ChapterAndVerse = ({ chapter, content, errorCode, verse, verseText }) => {
     }
 
     content = `<h1>${chapter.name_simple} - ${chapter.name_arabic}</h1>`
-        + `<h2>${chapter.chapter_number} - ${verse}</h2>`
+        + `<h2><a target="_blank" href="https://quran.com/${chapter.chapter_number}/${verse}">${chapter.chapter_number} - ${verse}</a></h2>`
         + `<p class="text-2xl leading-10 pr-10" dir="rtl">${verseText}</p>`
         + `<div><a target="_blank" href="https://corpus.quran.com/wordbyword.jsp?chapter=${chapter.chapter_number}&verse=${verse}">Corpus</a></div>`
         + content;
