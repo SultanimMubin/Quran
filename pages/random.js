7d5b1eaf4fd76e5b05a3a58caeff02cf8292a573
Saeed Nemati <sn.1361@gmail.com>
2022-04-07 15:19:05 +0430

    committed by script

--- pages/random.js
+++ pages/random.js
@@ -5,14 +5,16 @@ const Random = ({ chapter, verse }) => {
 
     const link = useRef()
 
-    const reload = () => {
+    const reload = (e) => {
         link.current.click()
         setTimeout(() => {
             window.location.reload()
         }, 1000)
+        e.stopPropagation()
+        e.preventDefault()
     }
 
-    return <div onClick={() => reload()} className="flex w-screen h-screen items-center justify-center">
+    return <div onClick={(e) => reload()} className="flex w-screen h-screen items-center justify-center">
         <a ref={link} className="text-5xl" target='_blank' href={`/${chapter}/${verse}`}>{chapter}:{verse}</a>
     </div>
 }
