9c79e3490bfbf6854c9b3f2de82bb4468456b41c
Saeed Nemati <sn.1361@gmail.com>
2022-04-09 10:35:16 +0430

    committed by script

--- pages/random.js
+++ pages/random.js
@@ -22,9 +22,9 @@ const Random = ({ chapter, verse }) => {
 export default Random
 
 export async function getServerSideProps({ params, res }) {
-    const randomChapter = Math.floor(Math.random() * 114)
+    const randomChapter = Math.ceil(Math.random() * 114)
     const verseCount = quran.filter(i => i.chapter * 1 === randomChapter)[0].verseCount
-    const randomVerse = Math.floor(Math.random() * verseCount)
+    const randomVerse = Math.ceil(Math.random() * verseCount)
     return {
         props: {
             chapter: randomChapter,
